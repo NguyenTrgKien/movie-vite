0d882f089fdd8fd9a3cb31c29d8182b6cccec7d0
TRUNGKIEN <nguyentrungkien040921@gmail.com>
2024-12-26 22:28:15 +0700

    update loading

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,10 +1,10 @@
 function Loading() {
     return (  
         <div className='w-full h-[100vh] fixed top-0 left-0 bg-black flex justify-center items-center'>
-            <div className='w-[6rem] h-[6rem] bg-[red] rounded-[50%] rotate-180 loadingAnimationLeft'>
+            <div className='w-[3rem] h-[3rem] md:w-[6rem] md:h-[6rem] bg-[red] rounded-[50%] rotate-180 loadingAnimationLeft'>
                 
             </div>
-            <div className='w-[6rem] h-[6rem] bg-[#2c3cee] rounded-[50%] rotate-180 loafingAnimationRight'>
+            <div className='w-[3rem] h-[3rem] md:w-[6rem] md:h-[6rem] bg-[#2c3cee] rounded-[50%] rotate-180 loafingAnimationRight'>
                 
             </div>
         </div>
